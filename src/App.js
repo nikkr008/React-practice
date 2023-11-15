71ff48b9a45d9c3edbd9168980d4aa89fd61782f
Nikhil Kumar <nik.kr008@gmail.com>
2023-11-15 12:10:32 +0530

    added lines

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar.js';
-import TextForm from './components/TextForm.js';
+//import TextForm from './components/TextForm.js';
+import About from './components/About.js';
 
 function App() {
   return (
     <>
     <Navbar title="TextUtils"/>
     <div className="container my-3">
-    <TextForm heading="Enter the text to analyze below"/>
+    {/*<TextForm heading="Enter the text to analyze below"/>*/}
+    <About/>
     </div>
     </>
   );
